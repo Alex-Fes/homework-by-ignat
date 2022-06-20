37073297775dc1b6e1c7627beb45053e1b482f2e
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-06-20 19:07:26 +0300

    complete HW3

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,26 +1,34 @@
-import React from 'react'
+import React, {ChangeEvent, KeyboardEvent, MouseEvent} from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+    name: string // need to fix any
+    setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void // need to fix any
+    addUser: (e: MouseEvent<HTMLButtonElement>) => void // need to fix any
+    error: string // need to fix any
+    totalUsers: number // need to fix any
+    onEnter: (e: KeyboardEvent<HTMLInputElement>) => void
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
-    {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
+    {name, onEnter, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
+
+    const inputClass = error ? s.errorInput : s.input// need to fix with (?:)
 
     return (
-        <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
+        <div className={s.greeting}>
+            <div>
+                <input value={name} onKeyDown={onEnter} onChange={setNameCallback} className={inputClass}
+                       onBlur={setNameCallback}/>
+                <div className={s.error}>{error}</div>
+            </div>
+
+
+                <button className={s.button} onClick={addUser} disabled={!name}>add</button>
+
+            <div className={s.count}>{totalUsers}</div>
         </div>
     )
 }
