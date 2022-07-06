81d26ba96cc7500fae0778c58b5b112b6b20471c
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-07-06 14:30:41 +0300

    Updates

---
+++ precache-manifest.dce4d44ef4a223a1d929f503f2a44f5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f32b419ed8283bbad5c33f437bafad",
+    "url": "/homework-by-ignat/index.html"
+  },
+  {
+    "revision": "0ab9d4f56c95ef8c37b2",
+    "url": "/homework-by-ignat/static/css/main.f253f896.chunk.css"
+  },
+  {
+    "revision": "a3a19f3debfee991ebd0",
+    "url": "/homework-by-ignat/static/js/2.d47564e7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/homework-by-ignat/static/js/2.d47564e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ab9d4f56c95ef8c37b2",
+    "url": "/homework-by-ignat/static/js/main.0a34a7d9.chunk.js"
+  },
+  {
+    "revision": "666643d5c9b8a8cc409c",
+    "url": "/homework-by-ignat/static/js/runtime-main.c989b24d.js"
+  },
+  {
+    "revision": "397792887f14c14bdd06c67e64a55047",
+    "url": "/homework-by-ignat/static/media/checked.39779288.png"
+  }
+]);
