b592ae2fd4a5b53ca5ad040cbbafb32b90e022b4
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-07-07 20:19:34 +0300

    begin HW5

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Pages";
+import style from "./Header.module.css";
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={style.main}>
+            <div className={style.nav}>
+                <input type="checkbox" id="hmt" className={style.hiddenMenuTicker}/>
+                <label className={style.btnMenu} htmlFor="hmt">
+                    <span className={style.first}></span>
+                    <span className={style.second}></span>
+                    <span className={style.third}></span>
+                </label>
+                <nav className={style.hiddenMenu}>
+                    <div className={style.item}><NavLink to={PATH.PRE_JUNIOR}>PRE_JUNIOR</NavLink></div>
+                    <div className={style.item}><NavLink to={PATH.JUNIOR}>JUNIOR</NavLink></div>
+                    <div className={style.item}><NavLink to={PATH.JUNIOR_PLUS}> JUNIOR +</NavLink></div>
+                </nav>
+            </div>
         </div>
     )
 }
---
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior-plus.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function JuniorPlus() {
+    return (
+        <div>
+            {/*<HW12/>*/}
+
+
+
+        </div>
+    )
+}
+
+export default JuniorPlus
+
