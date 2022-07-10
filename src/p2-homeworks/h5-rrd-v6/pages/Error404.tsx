92006bdf9d7414f485350a702ec95f6746519ae8
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-07-10 13:28:10 +0300

    add 404

--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -4,13 +4,16 @@ import './Error404.css'
 
 function Error404() {
     return (
-        <div >
-            <div className='main'>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+        <div>
+            <div className='div'>
+                <div className='main'>
+                    {/*<div>404</div>*/}
+                    {/*<div>Page not found!</div>*/}
+                    {/*<div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>*/}
+                </div>
             </div>
-        </div>
+    </div>
+
     )
 }
 
