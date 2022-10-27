d82d84fff076aebc2d99d99153b18b9b441a8958
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-10-27 15:56:58 +0300

    Updates

---
+++ precache-manifest.b02c0564167ec84cd7111b09d4a765cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b50f56502496042096704ef408289601",
+    "url": "/homework-by-ignat/index.html"
+  },
+  {
+    "revision": "5ba491f0ec555c679a9a",
+    "url": "/homework-by-ignat/static/css/main.ac5e30f7.chunk.css"
+  },
+  {
+    "revision": "2a0e5ff695a20f15ed97",
+    "url": "/homework-by-ignat/static/js/2.f8151d8f.chunk.js"
+  },
+  {
+    "revision": "9e5f57235bb7a84505af093094564e2d",
+    "url": "/homework-by-ignat/static/js/2.f8151d8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ba491f0ec555c679a9a",
+    "url": "/homework-by-ignat/static/js/main.28f0547d.chunk.js"
+  },
+  {
+    "revision": "666643d5c9b8a8cc409c",
+    "url": "/homework-by-ignat/static/js/runtime-main.c989b24d.js"
+  },
+  {
+    "revision": "397792887f14c14bdd06c67e64a55047",
+    "url": "/homework-by-ignat/static/media/checked.39779288.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/homework-by-ignat/precache-manifest.0ad81b10404ed32181754d9566483e40.js"
+  "/homework-by-ignat/precache-manifest.b02c0564167ec84cd7111b09d4a765cc.js"
 );
 
 self.addEventListener('message', (event) => {
